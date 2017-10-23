323919f5a4787f628cd6720a32190e1c8652530a
Jose Ramos <jar.arona@gmail.com>
2017-10-23 17:46:01 -0400

    changes

--- src/4functionCalc.kt
+++ src/4functionCalc.kt
@@ -1,3 +1,22 @@
 
+/*
+Jose Ramos
+10/23/17
 
-fun main (<args>)
+A simple four function calculator that will take in two numbers and an operand and
+output the outcome.
+ */
+fun main (args: Array<String>){
+
+    println("enter the two numbers followed by the operator which you wish to use.")
+
+   val num1 = readLine()!!.toDouble()
+   val num2 = readLine()!!.toDouble()
+   val operand = readLine()!!.toString()
+
+    println(num1 + "\n " + num2 + " \n" + operand)
+
+
+
+
+}
